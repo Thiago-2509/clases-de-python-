15adaeb03ff32bb4fb3b708a5da3c032ca49bf75
Thiago-2509 <thiagoupegui2509@gmail.com>
2026-08-05 11:09:39 -0500

    Realice un bucle para poder realizar mas compras

--- jugo.py
+++ jugo.py
@@ -1,28 +1,32 @@
 jugo = 5000
 agua = 2500
 cola = 5500
-print('''Opciones:
-      1. Agua: 2500
-      2. Jugo: 5000
-      3. Cola: 5500''')
-opcion = int(input('Que desea comprar (1-3): '))
-if opcion < 1 or opcion > 3:
-    print('Opcion no valida')
-    dinero = int(input('Ingrese su dinero'))
+continuar = 's'
+while continuar == 's':
+    print('''Opciones:
+        1. Agua: 2500
+        2. Jugo: 5000
+        3. Cola: 5500''')
+    opcion = int(input('Que desea comprar (1-3): '))
+    if opcion < 1 or opcion > 3:
+        print('Opcion no valida')
+        dinero = int(input('Ingrese su dinero'))
 
-if opcion == 1:
-    precio = agua
-    producto = 'agua'
-elif opcion == 2:
-    precio = jugo
-    producto = 'jugo'
-elif opcion == 3:
-    precio = cola
-    producto = 'cola'
+    if opcion == 1:
+        precio = agua
+        producto = 'agua'
+    elif opcion == 2:
+        precio = jugo
+        producto = 'jugo'
+    elif opcion == 3:
+        precio = cola
+        producto = 'cola'
 
-if dinero >= precio:
-    cambio = dinero - precio 
-    print(f'{producto} comprado')
-    print(f'Su cambio es: {cambio}')
-else:
-    print('Dinero insuficiente')
+    if dinero >= precio:
+        cambio = dinero - precio 
+        print(f'{producto} comprado')
+        print(f'Su cambio es: {cambio}')
+    else:
+        print('Dinero insuficiente')
+    continuar = int('Desea realizar otra compra? (s/n): ')
+print('Gracias por su compra')
