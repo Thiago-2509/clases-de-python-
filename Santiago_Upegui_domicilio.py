557e8c09d287574e5e0029d9d5a1053cdd7b56e9
Thiago-2509 <thiagoupegui2509@gmail.com>
2026-08-11 09:37:17 -0500

    Agregue comentarios que explican el codigo

--- Santiago_Upegui_domicilio.py
+++ Santiago_Upegui_domicilio.py
@@ -1,27 +1,27 @@
 #====================
 #Sistema de domicilio
 #====================
-# Aqui se ingresa el dato y con float se comvierte en decimal
+#Se agrega una variable y un while que sea 's' para que se puedan hacer varios calculos si el cliente lo decea y cuando se ingrese 'n' cierre el programa
 continuar = 's'
 while(continuar == 's'):
-
+    #Se agrega la variable distance_Km como float para asi poder agregar la distancia del domicilio, se hace en float para mas exactitud seguido de un print que diga la distancia agregada
     distance_Km = float(input('Ingrese la distancia en km: '))
 
     print(f'distancai en km:  {distance_Km}')
 
-    #Estas son las condiciones de el domicilio si es mayor o igal a tres muestra un valor y si es mayor o igual a 8 muestra otro valor. Si se pone una distancia mayor a 8 se muestr un mensaje que diga que no se cubre tanta distancia 
+    #Se agregan las condiciones planteadas para realizaar el calculo del domicilio creando una variable costo_domicilio para definir el precio, llegado el caso se ponga una distancia mayor la variable costo_domicilio valdra cero
     if distance_Km <= 3:
         costo_domicilio = 3000
     elif distance_Km <= 8:
         costo_domicilio = 7000
     else:
         costo_domicilio = 0
-
+    #Se hace otra condicion para decir el precio del domicilio y si costo_domicilio vale cero un print que diga 'Por fuera del area. No hay domicilio'
     if costo_domicilio == 0:
         print('Por fuera del area. No hay domicilio')
     else:
         print(f'El costo del domicilio es de: ${costo_domicilio}')  
-
+    #Se finaliza cerrando el while creando una variable continuar donde el usuario pueda decidir si hacer otro domicilio o no, si se pone no saldra un print que diga 'Gracias por utilizar el sistema'
     continuar = input('Realizar otro calculo (s/n): ')
 print('Gracias por utilizar el sistema')
         
